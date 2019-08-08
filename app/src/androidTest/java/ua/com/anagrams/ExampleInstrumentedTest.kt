be059c3cdfe39d2874cdd551f6096bf1e20fd544
Alexander Sheveryaev <alexsv.working@gmail.com>
2019-08-08 07:35:09 +0300

    add inspect permission in test spoon

--- app/src/androidTest/java/ua/com/anagrams/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ua/com/anagrams/ExampleInstrumentedTest.kt
@@ -1,6 +1,7 @@
 package ua.com.anagrams
 
 
+import android.Manifest
 import android.content.pm.ActivityInfo
 
 import androidx.test.espresso.Espresso.onView
@@ -11,22 +12,25 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Test
 import ua.com.anagrams.extensions.anagrams
 import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
 import com.squareup.spoon.Spoon
 import org.junit.Rule
+
 
 
 class ExampleInstrumentedTest {
     @Rule
     @JvmField
     var activityTestRule = ActivityTestRule(MainActivity::class.java)
-
+    @get:Rule var runtimePermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     @Test
     fun correct_input() {
+
         onView(withId(R.id.et_anagrams_revers)).perform(typeText("add12qw2"))
-        Spoon.screenshot(activityTestRule.activity, "before_click_button_revers")
+        Spoon.screenshot(activityTestRule.activity, "before")
         onView(withId(R.id.ib_revers)).perform(click())
-        Spoon.screenshot(activityTestRule.activity, "after_click_button_revers")
+        Spoon.screenshot(activityTestRule.activity, "after")
         onView(withId(R.id.tv_anagrams_result)).check(matches(withText("add12qw2".anagrams())))
         activityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         onView(withId(R.id.tv_anagrams_result)).check(matches(withText("add12qw2".anagrams())))
